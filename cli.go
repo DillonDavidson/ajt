f510b6a1c05e9b488f1893779d016a782d288d5b
DillonDavidson <90550872+DillonDavidson@users.noreply.github.com>
2026-08-02 18:25:48 -0700

    small cleanup

--- cli.go
+++ cli.go
@@ -7,25 +7,28 @@ import (
 
 func printUsage() {
 	fmt.Printf(
-		`ajt - Another Japanese Tool
+		`ajt - Utilities for managing video and subtitles files for AJATTing
 
-Usage: %s [OPTIONS]
+Usage:
+	%s <command> [options]
 
 Options:
 	audio-drop
-		for all mkv files, view the audio tracks and choose one to keep
+		Interactively keep audio one track in each MKV
 	condense
-		for all mkv files, condense into ogg files
-	rename [-dry-run] <file_extension...>
-		rename all <file_extension...> files numerically by extension type
-	shift <subtitle_file>
-		shift all of <subtitle_file>'s times
+		Condense all mkv files into ogg files
+	rename 
+		Rename all files numerically by extension type
+	shift 
+		Shift subtitle timestamps
 	sub-drop
-		for all mkv files, view the subtitle tracks and choose one to keep
-	sync [-dry-run] [-verbose] [-recursive]
-		for all mkv files with matching srt/ass file names, sync the external subtitles with the internal
+		Interactively keep one subtitle track in each MKV
+	sync 
+		Synchronize external subtitles with internal subtitles in each MKV
 	help
-		print this message
+		Show this message
+	version
+		Display version information
 `, os.Args[0],
 	)
 }
